d2e33ac26baac6cbdd50a7e002b618e915f2349b
Alex Li <5796630@mail.ru>
2019-08-11 13:19:13 +0300

    up

--- SOURCE/inc/gidrolock.h
+++ SOURCE/inc/gidrolock.h
@@ -7,23 +7,23 @@
 
 
 /* Define --------------------------------------------------------------------*/
-#define COLD_CLOSED                         GPIOD->BSRR = GPIO_BSRR_BR6  // M1
-#define COLD_OPEN                           GPIOD->BSRR = GPIO_BSRR_BS6  // M1
+#define COLD_CLOSED                         GPIOD->BSRR = GPIO_BSRR_BS6  // M1
+#define COLD_OPEN                           GPIOD->BSRR = GPIO_BSRR_BR6  // M1
 
-#define HOT_CLOSED                          GPIOC->BSRR = GPIO_BSRR_BR7  // M2
-#define HOT_OPEN                            GPIOC->BSRR = GPIO_BSRR_BS7  // M2
+#define HOT_CLOSED                          GPIOC->BSRR = GPIO_BSRR_BS7  // M2
+#define HOT_OPEN                            GPIOC->BSRR = GPIO_BSRR_BR7  // M2
 
-#define BOILER_CLOSED                       GPIOC->BSRR = GPIO_BSRR_BR6  // M3
-#define BOILER_OPEN                         GPIOC->BSRR = GPIO_BSRR_BS6  // M3
+#define BOILER_CLOSED                       GPIOC->BSRR = GPIO_BSRR_BS6  // M3
+#define BOILER_OPEN                         GPIOC->BSRR = GPIO_BSRR_BR6  // M3
 
-#define DIRECTLY_CLOSED                     GPIOB->BSRR = GPIO_BSRR_BR8  // M4
-#define DIRECTLY_OPEN                       GPIOB->BSRR = GPIO_BSRR_BS8  // M4
+#define DIRECTLY_CLOSED                     GPIOB->BSRR = GPIO_BSRR_BS8  // M4
+#define DIRECTLY_OPEN                       GPIOB->BSRR = GPIO_BSRR_BR8  // M4
 
-#define HEATING_CLOSED                      GPIOB->BSRR = GPIO_BSRR_BR9  // M5
-#define HEATING_OPEN                        GPIOB->BSRR = GPIO_BSRR_BS9  // M5
+#define HEATING_CLOSED                      GPIOB->BSRR = GPIO_BSRR_BS9  // M5
+#define HEATING_OPEN                        GPIOB->BSRR = GPIO_BSRR_BR9  // M5
 
-#define CLOSED                              GPIOE->BSRR = GPIO_BSRR_BR0  // M6
-#define OPEN                                GPIOE->BSRR = GPIO_BSRR_BS0  // M6
+#define CLOSED                              GPIOE->BSRR = GPIO_BSRR_BS0  // M6
+#define OPEN                                GPIOE->BSRR = GPIO_BSRR_BR0  // M6
 
 
 void BlockWater(void);
--- SOURCE/src/gidrolock.c
+++ SOURCE/src/gidrolock.c
@@ -6,6 +6,10 @@ void BlockWater(void){
   HOT_CLOSED;
   BOILER_CLOSED;
   HEATING_CLOSED;
+  CLOSED;
+  #if defined INFO
+    printf("\r\n********************************\r\n\r\n********* BLOCK WATER **********\r\n\r\n********************************\r\n");
+  #endif
 }
 
 
@@ -17,6 +21,8 @@ void GidrolockInit(void){
   GPIOB->CRH = (GPIOB->CRH & (~(GPIO_CRH_MODE8 | GPIO_CRH_CNF8))) | GPIO_CRH_MODE8; // M4
   GPIOB->CRH = (GPIOB->CRH & (~(GPIO_CRH_MODE9 | GPIO_CRH_CNF9))) | GPIO_CRH_MODE9; // M5
   GPIOE->CRL = (GPIOE->CRL & (~(GPIO_CRL_MODE0 | GPIO_CRL_CNF0))) | GPIO_CRL_MODE0; // M6
+  
+  
   
   #if defined DEBUG_GIDROLOCK
     printf("< OK >    Initialization CRANES\r\n");
